f716957257234ca3216e42019e6adab5491bfd55
Lubos Lenco <lubos.lenco@gmail.com>
2017-07-25 18:08:53 +0200

    Use Krom

--- Sources/Elements.hx
+++ Sources/Elements.hx
@@ -1,53 +1,69 @@
-package;
-import kha.Framebuffer;
-import kha.Assets;
+package ;
 
 import zui.*;
+import zui.Zui;
 import zui.Canvas;
 
 @:access(zui.Zui)
 class Elements {
 	var ui: Zui;
 	var cui: Zui;
-	var initialized = false;
-
 	var bg:kha.Image = null;
-
 	var canvas:TCanvas;
 
-	function onDrop(file:String) {
-		kha.LoaderImpl.loadImageFromDescription({ files: [file] }, function(image:kha.Image) {
-			var s = file.split("/");
-			var name = s[s.length - 1];
-			var asset:TAsset = { name: name, file: file, image: image };
-			canvas.assets.push(asset);
-		});
-	}
+	static inline var uiw = 240;
+	static inline var coff = 40;
+
+	var drag = false;
+	var assetNames:Array<String> = [];
+	var assetId = 0;
+	var dragAsset:TAsset = null;
 
 	public function new(canvas:TCanvas) {
-
 		this.canvas = canvas;
 
-		var _onDrop = onDrop;
-		untyped __js__("
-		document.ondragover = document.ondrop = (ev) => {
-			ev.preventDefault()
-		}
-		document.body.ondrop = (ev) => {
-			_onDrop(ev.dataTransfer.files[0].path);
-			ev.preventDefault()
-		}
-		");
+		// var _onDrop = onDrop;
+		// untyped __js__("
+		// document.ondragover = document.ondrop = (ev) => {
+		// 	ev.preventDefault()
+		// }
+		// document.body.ondrop = (ev) => {
+		// 	_onDrop(ev.dataTransfer.files[0].path);
+		// 	ev.preventDefault()
+		// }
+		// ");
 
-		Assets.loadEverything(loadingFinished);
+		kha.Assets.loadEverything(loaded);
 	}
 
-	function loadingFinished() {
-		initialized = true;
+	function loaded() {
 		var t = Reflect.copy(Themes.dark);
 		t.FILL_WINDOW_BG = true;
-		ui = new Zui({font: Assets.fonts.DroidSans, theme: t});
-		cui = new Zui({font: Assets.fonts.DroidSans, autoNotifyInput: false});
+		ui = new Zui({font: kha.Assets.fonts.DroidSans, theme: t, color_wheel: kha.Assets.images.color_wheel});
+		cui = new Zui({font: kha.Assets.fonts.DroidSans, autoNotifyInput: false});
+
+		kha.System.notifyOnDropFiles(function(filePath:String) {
+			filePath = StringTools.rtrim(filePath);			
+			importAsset(filePath);
+		});
+
+		kha.System.notifyOnRender(render);
+		kha.Scheduler.addTimeTask(update, 0, 1 / 60);
+	}
+
+	function importAsset(path:String) {
+		if (!StringTools.endsWith(path, ".jpg") &&
+			!StringTools.endsWith(path, ".png") &&
+			!StringTools.endsWith(path, ".hdr")) return;
+		
+		kha.LoaderImpl.loadImageFromDescription({ files: [path] }, function(image:kha.Image) {
+			var ar = path.split("/");
+			var name = ar[ar.length - 1];
+			var asset:TAsset = { name: name, file: path, image: image, id: assetId++ };
+			canvas.assets.push(asset);
+			assetNames.push(name);
+			hwin.redraws = 2;
+		});
 	}
 
 	function makeElem(type:ElementType) {
@@ -64,7 +80,7 @@ class Elements {
 			name = "Image";
 		}
 		var elem:TElement = {
-			id: 0,
+			id: Canvas.getElementId(canvas),
 			type: type,
 			name: name,
 			event: "",
@@ -81,11 +97,19 @@ class Elements {
 		return elem;
 	}
 
+	function getEnumTexts():Array<String> {
+		return assetNames.length > 0 ? assetNames : [""];
+	}
+
+	function getAssetIndex(asset:String):Int {
+		for (i in 0...canvas.assets.length) if (asset == canvas.assets[i].name) return i;
+		return 0;
+	}
+
 	var selectedElem = -1;
-	var handlewin = Id.handle();
-	var handleradio = Id.handle();
-	public function render(framebuffer: Framebuffer): Void {
-		if (!initialized) return;
+	var hwin = Id.handle();
+	var hradio = Id.handle();
+	public function render(framebuffer: kha.Framebuffer): Void {
 
 		if (bg == null) {
 			var w = kha.System.windowWidth();
@@ -106,8 +130,8 @@ class Elements {
 			}
 
 			bg.g2.color = 0xffffffff;
-			canvas.x = kha.System.windowWidth() - canvas.width - 20;
-			canvas.y = 40;
+			canvas.x = coff;
+			canvas.y = coff;
 			bg.g2.drawRect(canvas.x, canvas.y, canvas.width, canvas.height, 1.0);
 
 			bg.g2.end();
@@ -117,6 +141,7 @@ class Elements {
 
 		g.begin();
 
+		g.color = 0xffffffff;
 		g.drawImage(bg, 0, 0);
 
 		g.font = kha.Assets.fonts.DroidSans;
@@ -125,7 +150,7 @@ class Elements {
 		var titlew = g.font.width(40, title);
 		var titleh = g.font.height(40);
 		g.color = 0xffffffff;
-		g.drawString(title, kha.System.windowWidth() - titlew - 30, kha.System.windowHeight() - titleh - 10);
+		g.drawString(title, kha.System.windowWidth() - titlew - 30 - uiw, kha.System.windowHeight() - titleh - 10);
 		
 		Canvas.draw(cui, canvas, g);
 
@@ -134,15 +159,38 @@ class Elements {
 			var elem = canvas.elements[selectedElem];
 			g.color = 0xffffffff;
 			g.drawRect(canvas.x + elem.x, canvas.y + elem.y, elem.width, elem.height, 1);
+			g.drawRect(canvas.x + elem.x - 3, canvas.y + elem.y - 3, 6, 6, 1);
+			g.drawRect(canvas.x + elem.x - 3 + elem.width / 2, canvas.y + elem.y - 3, 6, 6, 1);
+			g.drawRect(canvas.x + elem.x - 3 + elem.width, canvas.y + elem.y - 3, 6, 6, 1);
+			g.drawRect(canvas.x + elem.x - 3, canvas.y + elem.y - 3 + elem.height / 2, 6, 6, 1);
+			g.drawRect(canvas.x + elem.x - 3 + elem.width, canvas.y + elem.y - 3 + elem.height / 2, 6, 6, 1);
+			g.drawRect(canvas.x + elem.x - 3, canvas.y + elem.y - 3 + elem.height, 6, 6, 1);
+			g.drawRect(canvas.x + elem.x - 3 + elem.width / 2, canvas.y + elem.y - 3 + elem.height, 6, 6, 1);
+			g.drawRect(canvas.x + elem.x - 3 + elem.width, canvas.y + elem.y - 3 + elem.height, 6, 6, 1);
 		}
 
 		g.end();
 
 		ui.begin(g);
-		// window() returns true if redraw is needed - windows are cached into textures
-		if (ui.window(handlewin, 0, 0, 240, 640, false)) {
+		if (ui.window(hwin, kha.System.windowWidth() - uiw, 0, uiw, kha.System.windowHeight(), false)) {
 
-			if (ui.panel(Id.handle({selected: true}), "CANVAS")) {
+			if (ui.button("Save")) {
+				// untyped __js__("const {dialog} = require('electron').remote");
+				// untyped __js__("console.log(dialog.showSaveDialog({properties: ['saveFile', 'saveDirectory']}))");
+				// untyped __js__("var fs = require('fs')");
+				// untyped __js__("fs.writeFileSync({0}, {1})", Main.prefs.path, haxe.Json.stringify(canvas));
+				
+				// Unpan
+				canvas.x = 0;
+				canvas.y = 0;
+				#if kha_krom
+				Krom.fileSaveBytes(Main.prefs.path, haxe.io.Bytes.ofString(haxe.Json.stringify(canvas)).getData());
+				#end
+				canvas.x = coff;
+				canvas.y = coff;
+			}
+
+			if (ui.panel(Id.handle({selected: false}), "CANVAS")) {
 				// ui.row([1/3, 1/3, 1/3]);
 				// if (ui.button("New")) {
 				// 	untyped __js__("const {dialog} = require('electron').remote");
@@ -154,19 +202,17 @@ class Elements {
 				// 	untyped __js__("console.log(dialog.showOpenDialog({properties: ['openFile', 'openDirectory', 'multiSelections']}))");
 				// }
 
-				if (ui.button("Save")) {
-					// untyped __js__("const {dialog} = require('electron').remote");
-					// untyped __js__("console.log(dialog.showSaveDialog({properties: ['saveFile', 'saveDirectory']}))");
-
-					untyped __js__("var fs = require('fs')");
-					untyped __js__("fs.writeFileSync({0}, {1})", Main.prefs.path, haxe.Json.stringify(canvas));
+				if (ui.button("New")) {
+					canvas.elements = [];
+					selectedElem = -1;
 				}
+
 				canvas.name = ui.textInput(Id.handle({text: canvas.name}), "Name", Right);
-				// ui.row([1/2, 1/2]);
-				// var strw = ui.textInput(Id.handle({text: canvas.width + ""}), "Width", Right);
-				// var strh = ui.textInput(Id.handle({text: canvas.height + ""}), "Height", Right);
-				// canvas.width = Std.parseInt(strw);
-				// canvas.height = Std.parseInt(strh);
+				ui.row([1/2, 1/2]);
+				var strw = ui.textInput(Id.handle({text: canvas.width + ""}), "Width", Right);
+				var strh = ui.textInput(Id.handle({text: canvas.height + ""}), "Height", Right);
+				canvas.width = Std.parseInt(strw);
+				canvas.height = Std.parseInt(strh);
 			}
 
 			ui.separator();
@@ -176,50 +222,70 @@ class Elements {
 				if (ui.button("Text")) {
 					var elem = makeElem(ElementType.Text);
 					canvas.elements.push(elem);
-					handleradio.position = canvas.elements.length - 1;
+					hradio.position = canvas.elements.length - 1;
 				}
 				if (ui.button("Image")) {
 					var elem = makeElem(ElementType.Image);
 					canvas.elements.push(elem);
-					handleradio.position = canvas.elements.length - 1;
+					hradio.position = canvas.elements.length - 1;
 				}
 				if (ui.button("Button")) {
 					var elem = makeElem(ElementType.Button);
 					canvas.elements.push(elem);
-					handleradio.position = canvas.elements.length - 1;
+					hradio.position = canvas.elements.length - 1;
 				}
 
-				var i = 0;
-				for (elem in canvas.elements) {
-					if (ui.radio(handleradio, i++, elem.name)) selectedElem = i - 1;
+				var i = canvas.elements.length - 1;
+				while (i >= 0) {
+					var elem = canvas.elements[i];
+					if (ui.radio(hradio, i, elem.name)) selectedElem = i;
+					i--;
 				}
-
-				if (canvas.elements.length > 0) {
-					if (ui.button("Remove")) {
-						canvas.elements.splice(selectedElem, 1);
-						if (selectedElem <= canvas.elements.length) {
-							selectedElem--;
-							handleradio.position = selectedElem;
-						}
-					}
+				ui.row([1/3, 1/3, 1/3]);
+				var temp1 = ui.t.BUTTON_BG_COL;
+				var temp2 = ui.t.BUTTON_BG_COL_HOVER;
+				var temp3 = ui.t.BUTTON_BG_COL_PRESSED;
+				ui.t.BUTTON_BG_COL = 0xff343436;
+				ui.t.BUTTON_BG_COL_HOVER = 0xff444446;
+				ui.t.BUTTON_BG_COL_PRESSED = 0xff303030;
+				var elems = canvas.elements;
+				if (ui.button("Up") && selectedElem < elems.length - 1) {
+					var t = canvas.elements[selectedElem];
+					canvas.elements[selectedElem] = canvas.elements[selectedElem + 1];
+					canvas.elements[selectedElem + 1] = t;
+					selectedElem++;
+					hradio.position = selectedElem;
 				}
+				if (ui.button("Down") && selectedElem > 0) {
+					var t = canvas.elements[selectedElem];
+					canvas.elements[selectedElem] = canvas.elements[selectedElem - 1];
+					canvas.elements[selectedElem - 1] = t;
+					selectedElem--;
+					hradio.position = selectedElem;
+				}
+				if (ui.button("Remove")) {
+					elems.splice(selectedElem, 1);
+					if (selectedElem == elems.length) selectedElem--;
+					else if (selectedElem < 0) selectedElem++;
+					hradio.position = selectedElem;
+				}
+				ui.t.BUTTON_BG_COL = temp1;
+				ui.t.BUTTON_BG_COL_HOVER = temp2;
+				ui.t.BUTTON_BG_COL_PRESSED = temp3;
 			}
 
 			ui.separator();
 
 			if (ui.panel(Id.handle({selected: true}), "PROPERTIES")) {
-				var i = selectedElem;
-				if (i >= 0) {
-					var elem = canvas.elements[i];
+				if (selectedElem >= 0) {
+					var elem = canvas.elements[selectedElem];
+					var id = elem.id;
 					ui.row([1/2, 1/2]);
-					elem.name = ui.textInput(Id.handle().nest(i, {text: elem.name}), "Name", Right);
-					elem.text = ui.textInput(Id.handle().nest(i, {text: elem.text}), "Text", Right);
+					elem.name = ui.textInput(Id.handle().nest(id, {text: elem.name}), "Name", Right);
+					elem.event = ui.textInput(Id.handle().nest(id, {text: elem.event}), "Event", Right);
 					ui.row([1/2, 1/2]);
-					elem.event = ui.textInput(Id.handle().nest(i, {text: elem.event}), "Event", Right);
-					elem.asset = ui.textInput(Id.handle().nest(i, {text: elem.asset}), "Asset", Right);
-					ui.row([1/2, 1/2]);
-					var handlex = Id.handle().nest(i, {text: elem.x + ""});
-					var handley = Id.handle().nest(i, {text: elem.y + ""});
+					var handlex = Id.handle().nest(id, {text: elem.x + ""});
+					var handley = Id.handle().nest(id, {text: elem.y + ""});
 					// if (drag) {
 						handlex.text = elem.x + "";
 						handley.text = elem.y + "";
@@ -229,15 +295,15 @@ class Elements {
 					elem.x = Std.parseFloat(strx);
 					elem.y = Std.parseFloat(stry);
 					ui.row([1/2, 1/2]);
-					var strw = ui.textInput(Id.handle().nest(i, {text: elem.width + ""}), "Width", Right);
-					var strh = ui.textInput(Id.handle().nest(i, {text: elem.height + ""}), "Height", Right);
+					var strw = ui.textInput(Id.handle().nest(id, {text: elem.width + ""}), "Width", Right);
+					var strh = ui.textInput(Id.handle().nest(id, {text: elem.height + ""}), "Height", Right);
 					elem.width = Std.int(Std.parseFloat(strw));
 					elem.height = Std.int(Std.parseFloat(strh));
-					ui.row([1/2, 1/2]);
-					var strcol = ui.textInput(Id.handle().nest(i, {text: "#ffffff"}), "Color", Right);
-					var stranch = ui.textInput(Id.handle().nest(i, {text: elem.anchor + ""}), "Anchor", Right);
-					elem.color = kha.Color.fromString(strcol);
-					elem.anchor = Std.int(Std.parseFloat(stranch));
+					elem.text = ui.textInput(Id.handle().nest(id, {text: elem.text}), "Text", Right);
+					elem.anchor = ui.combo(Id.handle().nest(id), ["None", "Top-Left", "Top-Center", "Top-Right"], "Anchor", true, Right);
+					var assetPos = ui.combo(Id.handle().nest(id, {position: getAssetIndex(elem.asset)}), getEnumTexts(), "Asset", true, Right);
+					elem.asset = getEnumTexts()[assetPos];
+					elem.color = Ext.colorWheel(ui, Id.handle().nest(id, {color: 0xffffff}), true, null, true);
 				}
 			}
 
@@ -245,23 +311,73 @@ class Elements {
 
 			if (ui.panel(Id.handle({selected: true}), "ASSETS")) {
 				if (canvas.assets.length > 0) {
-					for (i in 0...canvas.assets.length) {
+
+					var i = canvas.assets.length - 1;
+					while (i >= 0) {
 						var asset = canvas.assets[i];
-						ui.image(asset.image);
-						asset.name = ui.textInput(Id.handle().nest(i, {text: asset.name}), "Name", Right);
+						if (ui.image(asset.image) == State.Started) {
+							dragAsset = asset;
+						}
+						ui.row([7/8, 1/8]);
+						asset.name = ui.textInput(Id.handle().nest(asset.id, {text: asset.name}), "", Right);
+						assetNames[i] = asset.name;
+						if (ui.button("X")) {
+							asset.image.unload();
+							canvas.assets.splice(i, 1);
+							assetNames.splice(i, 1);
+						}
+						i--;
 					}
 				}
 				else {
-					ui.text("Drag & drop assets here");
+					ui.text("(Drag & drop assets)", zui.Zui.Align.Center, 0xff151515);
+					ui.text("(.png .jpg .hdr)", zui.Zui.Align.Center, 0xff151515);
 				}
 			}
 		}
 		ui.end();
+
+		g.begin(false);
+		if (dragAsset != null) {
+			var ratio = 128 / dragAsset.image.width;
+			var h = dragAsset.image.height * ratio;
+			g.drawScaledImage(dragAsset.image, ui.inputX, ui.inputY, 128, h);
+		}
+		g.end();
 	}
 
-	var drag = false;
+	function acceptDrag(index:Int) {
+		var elem = makeElem(ElementType.Image);
+		elem.asset = assetNames[index];
+		elem.x = ui.inputX - canvas.x;
+		elem.y = ui.inputY - canvas.y;
+		canvas.elements.push(elem);
+		hradio.position = canvas.elements.length - 1;
+	}
+
 	public function update() {
-		if (!initialized) return;
+
+		if (ui.inputReleased && dragAsset != null) {
+			if (ui.inputX < kha.System.windowWidth() - uiw) {
+				var index = 0;
+				for (i in 0...canvas.assets.length) if (canvas.assets[i] == dragAsset) { index = i; break; }
+				acceptDrag(index);
+			}
+			dragAsset = null;
+		}
+		if (dragAsset != null) return;
+
+		// Select elem
+		if (ui.inputStarted && ui.inputDownR) {
+			for (i in 0...canvas.elements.length) {
+				var elem = canvas.elements[i];
+				if (ui.inputX > canvas.x + elem.x && ui.inputX < canvas.x + elem.x + elem.width &&
+					ui.inputY > canvas.y + elem.y && ui.inputY < canvas.y + elem.y + elem.height) {
+					selectedElem = hradio.position = i;
+					break;
+				}
+			}
+		}
 
 		// Drag selected elem
 		if (selectedElem >= 0 && selectedElem < canvas.elements.length) {
@@ -277,7 +393,7 @@ class Elements {
 			}
 
 			if (drag) {
-				handlewin.redraws = 2;
+				hwin.redraws = 2;
 				elem.x += ui.inputDX;
 				elem.y += ui.inputDY;
 			}
--- Sources/Main.hx
+++ Sources/Main.hx
@@ -1,27 +1,26 @@
-package;
+package ;
 
-import kha.Scheduler;
-import kha.System;
 import zui.Canvas;
 
 class Main {
 
 	public static var prefs:Dynamic = null;
+	static var inst:Elements;
 
 	public static function main() {
-		System.init({ title : "ArmorUI", width : 1240, height : 640 }, initialized);
+		var w = kha.Display.width(0);
+		var h = kha.Display.height(0);
+		kha.System.init({ title : "ArmorUI", width : w, height : h, resizable: true }, initialized);
 	}
 	
-	private static function initialized(): Void {
+	static function initialized() {
 
 		kha.LoaderImpl.loadBlobFromDescription({ files: ["prefs.json"] }, function(blob:kha.Blob) {
 			prefs = haxe.Json.parse(blob.toString());
 
 			kha.LoaderImpl.loadBlobFromDescription({ files: [prefs.path] }, function(cblob:kha.Blob) {
 				var raw:TCanvas = haxe.Json.parse(cblob.toString());
-				var inst = new Elements(raw);
-				System.notifyOnRender(inst.render);
-				Scheduler.addTimeTask(inst.update, 0, 1 / 60);
+				inst = new Elements(raw);
 			});
 		});
 	}
