b20b26d4f46114edf5c06be78e5850837a459925
Moritz Brückner <mail@moritz-brueckner.de>
2019-12-21 22:55:59 +0100

    Refactor save procedure

--- Sources/arm2d/Assets.hx
+++ Sources/arm2d/Assets.hx
@@ -81,6 +81,66 @@ class Assets {
 		}
 	}
 
+	public static function save(canvas: TCanvas) {
+		// Unpan
+		canvas.x = 0;
+		canvas.y = 0;
+
+		saveCanvas(canvas);
+		saveAssets(canvas);
+		saveThemes();
+
+		canvas.x = Editor.coffX;
+		canvas.y = Editor.coffY;
+	}
+
+	static function saveCanvas(canvas: TCanvas) {
+		#if kha_krom
+		Krom.fileSaveBytes(Main.prefs.path, haxe.io.Bytes.ofString(haxe.Json.stringify(canvas)).getData());
+
+		#elseif kha_debug_html5
+		html5WriteFile(Main.prefs.path, haxe.Json.stringify(canvas));
+		#end
+	}
+
+	static function saveAssets(canvas: TCanvas) {
+		var filesPath = Main.prefs.path.substr(0, Main.prefs.path.length - 5); // .json
+		filesPath += '.files';
+
+		var filesList = '';
+		for (a in canvas.assets) filesList += a.file + '\n';
+
+		#if kha_krom
+		Krom.fileSaveBytes(filesPath, haxe.io.Bytes.ofString(filesList).getData());
+
+		#elseif kha_debug_html5
+		html5WriteFile(filesPath, filesList);
+		#end
+	}
+
+	static function saveThemes() {
+		var themesPath = haxe.io.Path.join([haxe.io.Path.directory(Main.prefs.path), "_themes.json"]);
+
+		#if kha_krom
+		Krom.fileSaveBytes(themesPath, haxe.io.Bytes.ofString(haxe.Json.stringify(Canvas.themes)).getData());
+
+		#elseif kha_debug_html5
+		html5WriteFile(themesPath, haxe.Json.stringify(Canvas.themes));
+		#end
+	}
+
+	#if kha_debug_html5
+	static function html5WriteFile(filePath: String, data: String) {
+		var fs = untyped __js__('require("fs");');
+		var path = untyped __js__('require("path")');
+
+		var filePath = path.resolve(untyped __js__('__dirname'), filePath);
+
+		try { fs.writeFileSync(filePath, data); }
+		catch (x: Dynamic) { trace('saving "${filePath}" failed'); }
+	}
+	#end
+
 	public static function getEnumTexts():Array<String> {
 		if(Main.inst==null) return [""];
 		return Editor.assetNames.length > 0 ? Editor.assetNames : [""];
